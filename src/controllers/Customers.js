6a022b851dbf2e63d4d93f1aa96937cbcc076bd4
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2023-02-11 20:52:51 -0300

    Bônus de limit para rota GET /customers pronta

--- src/controllers/Customers.js
+++ src/controllers/Customers.js
@@ -10,10 +10,10 @@ async function getCustomers(req, res) {
         if (error) {
             return res.status(errors["500.1"].code).send(errors["500.1"]);
         }
-        const { cpf, desc, offset, order } = req.query;
+        const { cpf, desc, limit, offset, order } = req.query;
         try {
             const allowedFieldsToOrder = ["name", "phone", "birthday"];
-            const query = await getPostgresClient().query(queries.select("*", "customers", cpf ? `"cpf" LIKE '${cpf.split(" ")[0]}%'` : null, order && allowedFieldsToOrder.includes(order) ? order : null, order && allowedFieldsToOrder.includes(order) && desc === "true" ? "DESC" : order && allowedFieldsToOrder.includes(order) ? "ASC" : null, !Number.isNaN(Number(offset)) ? Number.parseInt(offset) : null));
+            const query = await getPostgresClient().query(queries.select("*", "customers", cpf ? `"cpf" LIKE '${cpf.split(" ")[0]}%'` : null, order && allowedFieldsToOrder.includes(order) ? order : null, order && allowedFieldsToOrder.includes(order) && desc === "true" ? "DESC" : order && allowedFieldsToOrder.includes(order) ? "ASC" : null, !Number.isNaN(Number(limit)) ? Number.parseInt(limit) : null, !Number.isNaN(Number(offset)) ? Number.parseInt(offset) : null));
             releaseClient();
             return res.status(200).send(query.rows);
         } catch (error) {
