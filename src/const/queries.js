21cdbf3ef7d517cecee37dd6e58f8ff15aa694bf
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2023-02-11 21:57:33 -0300

    Correção de query de insert inválida

--- src/const/queries.js
+++ src/const/queries.js
@@ -1,6 +1,6 @@
 const queries = {
     select: (itemsToSelect, table, whereSelectCondition, orderBy, ascOrDesc, limit, offset) => (`SELECT ${itemsToSelect} FROM "${table}"${whereSelectCondition ? " WHERE " + whereSelectCondition : ""}${orderBy ? ` ORDER BY "${orderBy}"` : ""}${ascOrDesc ? ` ${ascOrDesc}` : ""}${limit ? ` LIMIT ${limit}` : ""}${offset ? ` OFFSET ${offset}` : ""};`),
-    insert: (table, tableValuesToInsert, values) => (`INSERT INTO "${table}" (${tableValuesToInsert}) VALUES (${values.map(value => Number.isInteger(Number(value)) && typeof value !== 'string' ? value ? value : "null" : new Date(value) instanceof Date && !isNaN(new Date(value)) ? `'${new Date(value).toISOString().split("T")[0]}'` : value `'${value}'`).toString()});`),
+    insert: (table, tableValuesToInsert, values) => (`INSERT INTO "${table}" (${tableValuesToInsert}) VALUES (${values.map(value => Number.isInteger(Number(value)) && typeof value !== 'string' ? value ? value : "null" : new Date(value) instanceof Date && !isNaN(new Date(value)) ? `'${new Date(value).toISOString().split("T")[0]}'` : `'${value}'`).toString()});`),
     update: (table, tableValuesToUpdate, condition) => (`UPDATE "${table}" SET ${tableValuesToUpdate.toString()} WHERE ${condition};`),
     delete: (table, whereDeleteCondition) => (`DELETE FROM "${table}" WHERE ${whereDeleteCondition};`),
 };
